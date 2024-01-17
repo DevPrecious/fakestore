d6a1103c0a8112d80b0a2cb784f2c7823c45f147
Precious Oladele <Oladelep77@gmail.com>
2024-01-17 23:22:08 +0100

    Refresh Indicator

--- lib/controllers/store_controller.dart
+++ lib/controllers/store_controller.dart
@@ -3,11 +3,13 @@ import 'dart:convert';
 import 'package:dartz/dartz.dart';
 import 'package:fakestore/models/product_model.dart';
 import 'package:fakestore/repository/store_repository.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class StoreController extends GetxController {
   final StoreRepository _repository = StoreRepository();
   Either<String, List<ProductModel>> products = right([]);
+  bool isLoading = false;
 
   @override
   void onInit() {
@@ -15,14 +17,26 @@ class StoreController extends GetxController {
     fetchProducts();
   }
 
-  void fetchProducts() async {
-    var result = await _repository.getProducts();
-    result.fold(
-      (error) => print("Error: $error"),
-      (data) {
-        products = right(data);
-        update();
-      },
-    );
+  void load() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      isLoading = !isLoading;
+      refresh();
+    });
+  }
+
+  fetchProducts() async {
+    try {
+      load();
+      var result = await _repository.getProducts();
+      result.fold(
+        (error) => print("Error: $error"),
+        (data) {
+          products = right(data);
+          update();
+        },
+      );
+    } finally {
+      load();
+    }
   }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -31,67 +31,83 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            GetBuilder<StoreController>(
-              init: _storeController,
-              builder: (controller) {
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    if (controller.products
-                        .isLeft()) // Check if there's an error
-                      Text(
-                          'Error: ${controller.products.fold((l) => l, (r) => '')}'),
-                    if (controller.products
-                        .isRight()) // Check if there's a list of products
-                      GridView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 8.0,
-                          mainAxisSpacing: 8.0,
-                        ),
-                        itemCount:
-                            controller.products.fold((l) => 0, (r) => r.length),
-                        itemBuilder: (context, index) {
-                          var product = controller.products.fold(
-                            (l) => null, // Error case, return null
-                            (r) => r[index],
-                          );
-
-                          if (product == null) {
-                            // Handle error case
-                            return Container();
-                          }
-
-                          // Build your product item UI here
-                          return Card(
-                            child: Column(
-                              children: [
-                                Image.network(
-                                  product.image ?? '',
-                                  height: 130.0,
-                                  width: double.infinity,
-                                ),
-                                Expanded(
-                                  child: Text(product.title ?? ''),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                      ),
-                  ],
-                );
+      body: GetBuilder<StoreController>(
+          init: _storeController,
+          builder: (controller) {
+            return RefreshIndicator(
+              onRefresh: () async {
+                await controller.fetchProducts();
               },
-            ),
-          ],
-        ),
-      ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    GetBuilder<StoreController>(
+                      init: _storeController,
+                      builder: (controller) {
+                        return controller.isLoading
+                            ? const Center(
+                                child: CircularProgressIndicator(),
+                              )
+                            : Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  if (controller.products
+                                      .isLeft()) // Check if there's an error
+                                    Text(
+                                        'Error: ${controller.products.fold((l) => l, (r) => '')}'),
+                                  if (controller.products
+                                      .isRight()) // Check if there's a list of products
+                                    GridView.builder(
+                                      shrinkWrap: true,
+                                      physics:
+                                          const NeverScrollableScrollPhysics(),
+                                      gridDelegate:
+                                          const SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 2,
+                                        crossAxisSpacing: 8.0,
+                                        mainAxisSpacing: 8.0,
+                                      ),
+                                      itemCount: controller.products
+                                          .fold((l) => 0, (r) => r.length),
+                                      itemBuilder: (context, index) {
+                                        var product = controller.products.fold(
+                                          (l) =>
+                                              null, // Error case, return null
+                                          (r) => r[index],
+                                        );
+
+                                        if (product == null) {
+                                          // Handle error case
+                                          return Container();
+                                        }
+
+                                        // Build your product item UI here
+                                        return Card(
+                                          child: Column(
+                                            children: [
+                                              Image.network(
+                                                product.image ?? '',
+                                                height: 130.0,
+                                                width: double.infinity,
+                                              ),
+                                              Expanded(
+                                                child:
+                                                    Text(product.title ?? ''),
+                                              ),
+                                            ],
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                ],
+                              );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
     );
   }
 }
